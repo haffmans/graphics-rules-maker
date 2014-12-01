f77cbed4b3ec342d04f1ae3dadfddb1eee074312
Wouter Haffmans <wouter@simply-life.net>
2014-12-01 22:23:57 +0100

    Add SC4 plugin Dutch translation

---
+++ translations/SimCity4GameWriter_nl.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL" sourcelanguage="en_US">
+<context>
+    <name>SimCity4Settings</name>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="76"/>
+        <source>SimCity 4 Settings</source>
+        <translation>SimCity 4 Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="78"/>
+        <source>Do not force low quality on Radeon HD 7000 series graphics cards (as opposed to the older Radeon 7000 series).</source>
+        <translation>Forceer geen lage kwaliteit op de serie Radeon HD 7000 grafische kaarten (in tegenstelling tot de oudere Radeon 7000 serie).</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="81"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Use this if you have a graphics card in the &lt;span style=&quot; font-weight:600;&quot;&gt;ATI Radeon HD 7000&lt;/span&gt; series.&lt;/p&gt;&lt;p&gt;The detection in the default code overlaps with the older &lt;span style=&quot; font-weight:600;&quot;&gt;Radeon 7000&lt;/span&gt; series, and forces the settings to a low quality code. Apply this tweak to make sure the detection only picks out the old graphics cards, but not the newer &lt;span style=&quot; font-weight:600;&quot;&gt;HD&lt;/span&gt; ones.&lt;/p&gt;&lt;p&gt;If you do not have an ATI/AMD graphics card, this option does nothing.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Gebruik dit als je een grafische kaart hebt in de &lt;span style=&quot; font-weight:600;&quot;&gt;ATI Radeon HD 7000&lt;/span&gt; serie.&lt;/p&gt;&lt;p&gt;De detectie in het spel overlapt met de oudere &lt;span style=&quot; font-weight:600;&quot;&gt;Radeon 7000&lt;/span&gt; serie en forceert daardoor lage kwaliteitsinstellingen. Deze instelling toepassen zorgt ervoor dat dit alleen bij de oudere kaarten gebeurt, maar niet bij de nieuwere &lt;span style=&quot; font-weight:600;&quot;&gt;HD&lt;/span&gt; varianten.&lt;/p&gt;&lt;p&gt;Als je geen ATI/AMD kaart hebt, heeft deze instelling geen effect.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="83"/>
+        <source>Fix AMD Radeon HD 7000 series recognition</source>
+        <translation>Repareer AMD Radeon HD 7000 serie herkenning</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="85"/>
+        <source>Detect any graphics card as a fast one.</source>
+        <translation>Detecteer alle grafische kaarten als snel.</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="88"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Adds a wildcard match for any graphics card as fast cards, enabling highest quality settings. Only some old GeForce cards are explicitly excluded.&lt;/p&gt;&lt;p&gt;Use this if you do not get all the high quality graphics in your game, such as shadows, fog etc.&lt;/p&gt;&lt;p&gt;Note that your CPU speed and memory may still influence this setting.&lt;/p&gt; &lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Zorgt ervoor dat alle grafische kaarten als snel gedetecteerd worden, om zo hogere kwaliteitsinstellingen in te kunnen schakelen. Alleen enkele oudere GeForce kaarten blijven expliciet uitgesloten.&lt;/p&gt;&lt;p&gt;Gebruik dit als je niet alle hoge kwaliteitsopties krijgt in je spel, zoals schaduwen, mist, enzovoorts.&lt;/p&gt;&lt;p&gt;Opmerking: je CPU-snelheid en geheugen kunnen nog steeds deze instelling beïnvloeden.&lt;/p&gt; &lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="90"/>
+        <source>Force card as fast graphics card</source>
+        <translation>Forceer kaart als snelle kaart</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="92"/>
+        <source>Disable all tweaks and restore default values.</source>
+        <translation>Zet alle instellingen terug naar de standaardwaarden.</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="95"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Reset all settings to their default values.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Zet alle instellingen terug naar de standaardwaarden&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="97"/>
+        <source>&amp;Default Settings</source>
+        <translation>Standaard&amp;instellingen</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="99"/>
+        <source>Always enable all high quality settings in hardware mode, bypassing graphics card detection.</source>
+        <translation>Schakel altijd alle hoge-kwaliteitsinstellingen in bij hardware-modues. Dit omzeilt alle standaardregels om ondersteuning te detecteren.</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="102"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Forces all settings to be available in hardware rendering mode, regardless of the detected capabilities of your card. In particular, this means you get:&lt;/p&gt;&lt;p&gt;- high cars/sims detail&lt;/p&gt;&lt;p&gt;- high quality shadows&lt;/p&gt;&lt;p&gt;- clouds, fog and waves on&lt;/p&gt;&lt;p&gt;- translucency on&lt;/p&gt;&lt;p&gt;- colored cursor,&lt;/p&gt;&lt;p&gt;- maximum screen size&lt;/p&gt;&lt;p&gt;- 32 bit colors&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;This may cause performance problems, so use with care!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Forceert dat alle instellingen beschikbaar zijn in hardware rendering modus, ongeacht de gedetecteerde mogelijkheden van je kaart. Dit betekent dat je de volgende opties krijgt:&lt;/p&gt;&lt;p&gt;- hoog detail auto&apos;s/sims&lt;/p&gt;&lt;p&gt;- hoge kwaliteit schaduwen&lt;/p&gt;&lt;p&gt;- wolken, mist en golven aan&lt;/p&gt;&lt;p&gt;- transparantie aan&lt;/p&gt;&lt;p&gt;- gekleurde cursor,&lt;/p&gt;&lt;p&gt;- maximale schermgrootte&lt;/p&gt;&lt;p&gt;- 32-bits kleuren&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;p&gt;Dit kan prestatieproblemen opleveren, dus wees voorzichting in het gebruik!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../build/GraphicRulesMaker/src/plugins/simcity4/ui_simcity4settings.h" line="104"/>
+        <source>Force high quality settings</source>
+        <translation>Forceer hoge kwaliteit</translation>
+    </message>
+</context>
+</TS>
